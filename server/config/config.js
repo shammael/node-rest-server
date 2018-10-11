b3dee1b1f9ff795a6db5cd4190a6e17de6bf9b60
Shammael <shammamnd2015@gmail.com>
2018-10-10 23:06:22 -0400

    config MONGO_URI

--- server/config/config.js
+++ server/config/config.js
@@ -8,10 +8,10 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
 let urlDB = '';
 
 if(process.env.NODE_ENV === 'dev'){
-	urlDB = 'mongodb://localhost:27017/cafe2'
+	urlDB = 'mongodb://localhost:27017/cafe2';
 }
 else{
-	urlDB = 'mongodb://cafe2-user:waleska2017@ds129393.mlab.com:29393/cafe2'
+	urlDB = process.env.MONGO_URI;
 }
 
 process.env.URLDB = urlDB;
